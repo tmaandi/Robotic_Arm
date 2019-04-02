f6929864b1e33c179c15c4cfbb0caa2809c2cc26
Tarun Maandi <tmaandi@umich.edu>
2019-04-02 03:45:03 +0100

    Added analog input channels

--- ArmPosFeedback.py
+++ ArmPosFeedback.py
@@ -15,10 +15,13 @@ cs = digitalio.DigitalInOut(board.D22)
 # create the mcp object
 mcp = MCP.MCP3008(spi, cs)
 
-# create an analog input channel on pin 0
+# create an analog input channels on pins 0-3
 chan0 = AnalogIn(mcp, MCP.P0)
+chan1 = AnalogIn(mcp, MCP.P1)
+chan2 = AnalogIn(mcp, MCP.P2)
+chan3 = AnalogIn(mcp, MCP.P3)
 
 if __name__ == "__main__":
     while(1):
-        print(('Raw ADC Value: ', chan0.value))
-        print(('ADC Voltage: ' + str(chan0.voltage) + 'V'))
+        print(('Raw ADC Value: ', [chan0.value,chan1.value,chan2.value,chan3.value] ))
+        print(('ADC Voltage: ' + str([chan0.voltage,chan1.voltage,chan2.voltage,chan3.voltage]) + 'V'))
